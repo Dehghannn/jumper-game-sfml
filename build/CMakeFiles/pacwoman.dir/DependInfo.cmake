4f33cd0cbd83d01b3ac4e767f46f4c493cd800b6
Ali Dehghanzade <ali.dehghanzade76@gmail.com>
2021-03-09 23:20:52 +0330

    added gitignore

--- build/CMakeFiles/pacwoman.dir/DependInfo.cmake
+++ build/CMakeFiles/pacwoman.dir/DependInfo.cmake
@@ -4,7 +4,7 @@ set(CMAKE_DEPENDS_LANGUAGES
   )
 # The set of files for implicit dependencies of each language:
 set(CMAKE_DEPENDS_CHECK_CXX
-  "/home/dehghannn/Documents/Code/Cpp/pac-woman-sfml/src/main.cpp" "/home/dehghannn/Documents/Code/Cpp/pac-woman-sfml/build/CMakeFiles/pacwoman.dir/src/main.cpp.o"
+  "/home/dehghannn/Documents/Code/Cplusplus/Pac-Woman/src/main.cpp" "/home/dehghannn/Documents/Code/Cplusplus/Pac-Woman/build/CMakeFiles/pacwoman.dir/src/main.cpp.o"
   )
 set(CMAKE_CXX_COMPILER_ID "GNU")
 
